def778d5480f9663c46fa6cb8f66bbc106eee0cf
Dalyn Baldwin <dlbaldwi@byu.edu>
2023-01-14 10:31:07 -0700

    Comments and organization fixed. Fully functional

--- mission2/Die.cs
+++ mission2/Die.cs
@@ -6,10 +6,12 @@ namespace mission2
 {
     class Die
     {
-        // Elements of an individual Die
-        // Random number from 1 to 6 for the value
+
+    // Elements of an individual Die
         public int GetVal()
         {
+
+        // Random number from 1 to 6 for the value and return
             var rand = new Random();
             int die = rand.Next(1, 7);
             return die;
--- mission2/Roll.cs
+++ mission2/Roll.cs
@@ -6,17 +6,21 @@ namespace mission2
 {
     class Roll
     {
-        // Defines what a roll is
-        // Creates an instace of 2 dice (a and b), adds them together and return the roll total
+
+    // Defines what a roll is
+    // Creates an instace of 2 dice (a and b), adds them together and return the roll total
         public int Total()
         {
             int roll;
 
-            Die dieA = new Die();     // Instance of die A
-            Die dieB = new Die();     // Instance of die B
+        // Instance of die A
+            Die dieA = new Die();
 
+        // Instance of die B
+            Die dieB = new Die();
+
+        // Calculate roll value and return
             roll = dieA.GetVal() + dieB.GetVal();
-
             return roll;
         }
     }

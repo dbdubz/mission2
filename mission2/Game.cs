2e0a123155e13f2f954f64a6892dd2820c7de1ab
Dalyn Baldwin <dlbaldwi@byu.edu>
2023-01-14 11:48:49 -0700

    Minimized Main() workload and set up values to be inserted into a list, not dictionary

---
+++ mission2/Game.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mission2
+{
+    class Game
+    {
+        public static int Start()
+        {
+            int nRolls;
+
+            // Game Start print
+            Console.WriteLine("Welcome to the dice throwing simulator!\n");
+            Console.Write("How many dice rolls would you like to simulate? ");
+
+            // Get number of rolls and play the game
+            nRolls = (int)Convert.ToInt64(Console.ReadLine());
+
+            return nRolls;
+        }
+        public static Dictionary<int, string> Play(int nRolls)
+        { 
+            int rollTotal;
+            int val;
+            int percentage;
+            int n;
+            int cRoll = 0;
+            int possible = 2;
+            Dictionary<int, string> outcomes = new Dictionary<int, string>();
+
+            while (possible < 13)
+            {
+                outcomes.Add(possible, "");
+                possible += 1;
+            }
+
+            int[] rollVals = new int[nRolls];
+
+            // Perform the rolls
+            while (cRoll < nRolls)
+            {
+
+                // Creates a roll instance
+                Roll roll = new Roll();
+
+                // Calls the Total method
+                rollTotal = roll.Total();
+                rollVals[cRoll] = rollTotal;
+                cRoll += 1;
+            }
+
+            possible = 2;
+            
+            while (possible < 13)
+            {
+                val = rollVals.Count(r => r == possible);
+
+                // Convert the roll % to an int, rounded
+                percentage = (int)Math.Round(((val / (decimal)nRolls) * 100), 0);
+                n = 0;
+
+                // Convert the percentages to *
+                while (n < percentage)
+                {
+                    outcomes[possible] += "*";
+                    n += 1;
+                }
+
+                possible += 1;
+            }
+
+            return outcomes;
+        }
+
+        public static void End(int nRolls, Dictionary<int, string> outcomes)
+        {
+            // Print Results header
+            Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine($"Total number of rolls = {nRolls}.\n");
+
+
+            // Print the output, omitting 1 as a val because it is impossible
+            foreach (KeyValuePair<int, string> x in outcomes)
+            {
+                int key = x.Key;
+                string s = x.Value;
+
+                if (key < 10)
+                {
+
+                    // Format 2-9 so they are flush with 10-12
+                    Console.WriteLine($"0{key}: {s}");
+                }
+                else
+                {
+                    Console.WriteLine($"{key}: {s}");
+                }
+            }
+
+            // End game
+            Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
--- mission2/Program.cs
+++ mission2/Program.cs
@@ -7,87 +7,9 @@ namespace mission2
     {
         static void Main(string[] args)
         {
-
-        // Variable declarations
-            int nRolls;
-            int rollTotal;
-            int cRoll = 0;
-            int possible = 2;
-            Dictionary<int, int> rollCount = new Dictionary<int, int>();
-            Dictionary<int, string> outcomes = new Dictionary<int, string>();
-
-        // Set up possible roll counts for ending output.
-        // Chose a dictionary because it seemed like the simplist option for doing this
-            while (possible < 13)
-            {
-                rollCount.Add(possible, 0);
-                outcomes.Add(possible, "");
-                possible += 1;
-            }
-
-        // Game Start print
-            Console.WriteLine("Welcome to the dice throwing simulator!\n");
-            Console.Write("How many dice rolls would you like to simulate? ");
-
-        // Get number of rolls
-            nRolls = (int)Convert.ToInt64(Console.ReadLine());
-
-        // Perform the rolls
-            while (cRoll < nRolls)
-            {
-
-            // Creates a roll instance
-                Roll roll = new Roll();
-
-            // Calls the Total method
-                rollTotal = roll.Total();
-                rollCount[rollTotal] += 1;
-                cRoll += 1;
-            }
-
-        // Print Results header
-            Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
-            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
-            Console.WriteLine($"Total number of rolls = {nRolls}.\n");
-
-        // Get % of rolls for each value
-            foreach (KeyValuePair<int, int> n in rollCount)
-            {
-                int key = n.Key;
-                int val = n.Value;
-
-            // Convert the roll % to an int, rounded
-                int p = (int)Math.Round(((val / (decimal)nRolls) * 100), 0);
-                int x = 0;
-
-            // Convert the percentages to *
-                while (x < p)
-                {
-                    outcomes[key] += "*";
-                    x += 1;
-                }
-            }
-
-        // Print the output, omitting 1 as a val because it is impossible
-            foreach (KeyValuePair<int, string> n in outcomes)
-            {
-                int key = n.Key;
-                string val = n.Value;
-
-                if (key < 10)
-                {
-
-                // Format 2-9 so they are flush with 10-12
-                    Console.WriteLine($"0{key}: {val}");
-                }
-                else
-                {
-                    Console.WriteLine($"{key}: {val}");
-                }
-            }
-
-        // End game
-            Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+            int nRolls = Game.Start();
+            Dictionary<int, string> outcomes = Game.Play(nRolls);
+            Game.End(nRolls, outcomes);
         }
     }
 }

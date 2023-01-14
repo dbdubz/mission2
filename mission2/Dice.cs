f198e2b88e01b4be846677ba340968c9144c0216
Dalyn Baldwin <dlbaldwi@byu.edu>
2023-01-13 20:52:51 -0700

    created dice system and began main program

---
+++ mission2/Dice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mission2
+{
+    class Dice
+    {
+        private static int Die()
+        {
+            int[] sides = { 1, 2, 3, 4, 5, 6 };
+            List<int> options = new List<int>(sides);
+            var rand = new Random();
+
+            int die = rand.Next(options.Count);
+
+            return die;
+        }
+
+        public static int Roll()
+        {
+            int roll;
+            int dieA;
+            int dieB;
+
+            dieA = Die();
+            dieB = Die();
+
+            roll = dieA + dieB;
+
+            return roll;
+        }
+    }
+}
--- mission2/Program.cs
+++ mission2/Program.cs
@@ -5,8 +5,19 @@ namespace mission2
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        { 
+            int nRolls;
+            int rollTotal;
+            int cRoll = 0;
+            Console.WriteLine("Welcome to the dice throwing simulator!\n");
+            Console.Write("How many dice rolls would you like to simulate? ");
+            nRolls = Convert.ToInt32(Console.ReadLine());
+
+            while (cRoll < nRolls)
+            {
+                rollTotal = Dice.Roll();
+
+            }
         }
     }
 }
